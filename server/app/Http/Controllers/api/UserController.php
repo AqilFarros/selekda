b0e33ed294ff3004119fb5735197f98ff8eae1eb
AqilFarros <aqilghonim@gmail.com>
2024-08-30 16:14:21 +0700

    mulai server

---
+++ server/app/Http/Controllers/api/UserController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function register() {}
+
+    public function login() {}
+
+    public function updateProfile() {}
+}
--- server/app/Models/User.php
+++ server/app/Models/User.php
@@ -18,8 +18,13 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'username',
         'email',
         'password',
+        'birth',
+        'phone',
+        'picture',
+        'date',
     ];
 
     /**
