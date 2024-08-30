de6bf28373d76f90e5b907a3b93d243272eb3ae4
AqilFarros <aqilghonim@gmail.com>
2024-08-30 09:27:13 +0700

    admin dashboard

--- design/main.js
+++ design/main.js
@@ -1,3 +1,7 @@
 const toggleNavbar = () => {
-    document.getElementById("navigation").classList.toggle('active')
+    document.getElementById("navigation").classList.toggle('active');
+}
+
+const toggleSidebar = () => {
+    document.getElementById("sidebar").classList.toggle('active');
 }
